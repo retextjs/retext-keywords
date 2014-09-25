3d00f81e6760dd3f740bbd34392ce5b3ca1f6008
wooorm <tituswormer@gmail.com>
2014-09-25 23:06:52 +0200

    Refactor benchmark
    Includes updates for retext@0.2.0.

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,75 +1,82 @@
 'use strict';
 
-var Retext, retext, keywords, source,
-    sourceSmall, sourceMedium,
-    tiny, small, medium,
-    wordCount, sentenceCount, paragraphCount;
+/**
+ * Dependencies.
+ */
+
+var Retext,
+    keywords;
 
 Retext = require('retext');
 keywords = require('..');
 
-/* First paragraph on term extraction from Wikipedia:
- * http://en.wikipedia.org/wiki/Terminology_extraction
+/**
+ * Fixtures.
+ *
+ * First paragraph on term extraction from Wikipedia:
+ *
+ *   http://en.wikipedia.org/wiki/Terminology_extraction
  */
-source = 'Terminology mining, term extraction, term recognition, or ' +
+
+var source,
+    sourceSmall,
+    sourceMedium;
+
+source =
+    'Terminology mining, term extraction, term recognition, or ' +
     'glossary extraction, is a subtask of information extraction. ' +
     'The goal of terminology extraction is to automatically extract ' +
     'relevant terms from a given corpus.\n\n';
 
-/* Test data */
 sourceSmall = Array(11).join(source);
 sourceMedium = Array(11).join(sourceSmall);
 
+/**
+ * Retext.
+ */
+
+var retext;
+
 retext = new Retext().use(keywords);
 
-tiny = retext.parse(source);
-small = retext.parse(sourceSmall);
-medium = retext.parse(sourceMedium);
+/**
+ * Benchmarks.
+ */
 
-wordCount = sentenceCount = paragraphCount = 0;
+suite('A big section (10 paragraphs)', function () {
+    var tree;
 
-tiny.visitType(tiny.WORD_NODE, function () {
-    wordCount++;
-});
-
-tiny.visitType(tiny.SENTENCE_NODE, function () {
-    sentenceCount++;
-});
-
-tiny.visitType(tiny.PARAGRAPH_NODE, function () {
-    paragraphCount++;
-});
-
-if (wordCount !== 30) {
-    console.error('Word count should be 300!');
-}
-
-if (sentenceCount !== 2) {
-    console.error('Sentence count should be 300!');
-}
-
-if (paragraphCount !== 1) {
-    console.error('Paragraph count should be 300!');
-}
-
-/* Benchmarks */
-suite('Finding keywords in English', function () {
-    bench('small (10 paragraphs, 20 sentences, 300 words)', function () {
-        small.keywords();
+    before(function(next) {
+        retext.parse(sourceSmall, function (err, node) {
+            tree = node;
+            next();
+        });
     });
 
-    bench('medium (100 paragraphs, 200 sentences, 3000 words)', function () {
-        medium.keywords();
+    bench('Finding keywords', function () {
+        tree.keywords();
+    });
+
+    bench('Finding keyphrases', function () {
+        tree.keyphrases();
     });
 });
 
-/* Benchmarks */
-suite('Finding keyphrases in English', function () {
-    bench('small (10 paragraphs, 20 sentences, 300 words)', function () {
-        small.keyphrases();
+suite('A big article (100 paragraphs)', function () {
+    var tree;
+
+    before(function(next) {
+        retext.parse(sourceMedium, function (err, node) {
+            tree = node;
+            next();
+        });
     });
 
-    bench('medium (100 paragraphs, 200 sentences, 3000 words)', function () {
-        medium.keyphrases();
+    bench('Finding keywords', function () {
+        tree.keywords();
+    });
+
+    bench('Finding keyphrases', function () {
+        tree.keyphrases();
     });
 });
