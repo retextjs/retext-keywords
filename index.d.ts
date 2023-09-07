7713a9a7ec614f2099fda108ab9999e87b8ebf9d
Titus Wormer <tituswormer@gmail.com>
2023-09-07 13:54:30 +0200

    Refactor to move implementation to `lib/`

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
-// Need a random export to turn this into a module?
-import type {Keyword, Keyphrase} from './index.js'
+import type {Keyword, Keyphrase} from './lib/index.js'
+
+export type {
+  Keyphrase,
+  Keyword,
+  KeywordMatch,
+  Options,
+  PhraseMatch
+} from './lib/index.js'
+
+export {default} from './lib/index.js'
 
 declare module 'vfile' {
   interface DataMap {
